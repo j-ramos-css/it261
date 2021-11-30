090a6eab5bcd002ee0aaf7b32960cfe9969b1cc1
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-11-29 18:13:19 -0800

    index

--- index.php
+++ index.php
@@ -75,10 +75,7 @@
 			<h3>Week 8</h3>
 
 			<ul>
-				<li><a href="weeks/week8/">form1.php</a></li>
-				<li><a href="weeks/week8/">functions.php</a></li>
-				<li><a href="weeks/week8/">strings.php</a></li>
-
+				<li><a href="weeks/week8/">people.php</a></li>
 			</ul>
 			
 		</aside>
