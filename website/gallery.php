6e6bef84749479c7bd5c81c1c015d4f548c63d68
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-12-01 19:53:44 -0800

    gallery

--- website/gallery.php
+++ website/gallery.php
@@ -1,4 +1,4 @@
-<?php include '../includes/websites-header.php';
+<?php 
 // cat gallery
 
 $cats['Maine_Coon'] = 'maine_One of the biggest domestic cats.';
@@ -16,64 +16,85 @@ $cats['Scottish_Fold'] = 'sfold_A breed with a natural mutation that affects car
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="robots" content="noindex,nofollow" />
 	<meta charset="utf-8">
-	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+	<link
+		href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+		rel="stylesheet">
 	<script src="https://use.fontawesome.com/6a71565c22.js"></script>
-	<link rel="stylesheet" href="../css/nav.css" />
-	<link rel="stylesheet" href="../css/portal.css" />
+	<link rel="stylesheet" href="styles/styles.css"/>
 
-    <style>
-        body {
-            background-color: linen;
-        }
+	<style>
+		body {
+			background-color: linen;
+		}
 
-        table {
-            border: 5px solid maroon;
-            color: maroon;
-            margin: 50px auto;
-            padding: 8px;
-        }
+		table {
+			border: 5px solid maroon;
+			color: maroon;
+			margin: 50px auto;
+			padding: 8px;
+		}
 
-        td {
-            border: 2px solid maroon;
-        }
-
-    </style>
+		td {
+			border: 2px solid maroon;
+		}
+	</style>
 
 </head>
 
 
 <body>
-	<!-- START WRAPPER -->
-	<main class="wrapper">
-		<header>
-			<h1 id="header"><a href="../index.php">Julio Ramos | IT261 Portal</a></h1>
+	<header>
+
+		<div class="header-inner">
+			<a href="index.html">
+				<img id="logo" src="images/logo.png" alt="logo">
+			</a>
+
 			<nav>
-				<ul class="topnav" id="myTopnav">
-
-					<li><a href="../index.php" >Home</a></li>
-					<li><a href="">Switch (3)</a></li>
-					<li><a href="adder.php">Troubleshoot (4)</a></li>
-					<li><a href="calculator.php">Calculator (5)</a></li>
-					<li><a href="email.php">Email (6)</a></li>
-					<li><a href="gallery.php">Gallery (7)</a></li>
-					<li><a href="databases.php">Databases (8)</a></li>
-					<li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
-
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="">About</a></li>
+					<li><a href="">Daily</a></li>
+					<li><a href="">Project</a></li>
+					<li><a href="">Contact</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
 				</ul>
 			</nav>
-		</header>
+		</div> <!-- Close header inner -->
+	</header>
+
+	<table>
+		<?php foreach($cats as $name => $image) : ?>
+		<tr>
+			<td><img src="images/<?php echo substr($image, 0, 5); ?>.jpg"
+					alt="<?php echo str_replace('_', ' ', $name); ?>" width="400" height="400"></td>
+			<td><?php echo str_replace('_', ' ', $name); ?></td>
+			<td><?php echo substr($image, 6, 100); ?></td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
+
+</body>
+
+<!-- BEGINNING OF FOOTER -->
+
+<footer>
+	<ul>
+		<li>Copyright &copy; 2021</li>
+		<li>All rights reserved</li>
+		<li><a href="">Terms of Use</li>
+		<li><a href="">Web Design by Julio</li>
+		<li><a href="">HTML Validation</li>
+		<li><a href="">CSS Validation</li>
 
 
+	</ul>
+</footer>
 
-    <table>
-        <?php foreach($cats as $name => $image) : ?>
-        <tr>
-            <td><img src="images/<?php echo substr($image, 0, 5); ?>.jpg" alt="<?php echo str_replace('_', ' ', $name); ?>" width="400" 
-     height="400" ></td>
-            <td><?php echo str_replace('_', ' ', $name); ?></td>
-            <td><?php echo substr($image, 6, 100); ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
 
-<?php include '../includes/footer.php'; ?>
+</div>
+<!--end wrapper-->
+
+</body>
+
+</html>
