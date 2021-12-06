df29aed7a79f14f160cfc7dfb257d1f495841695
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-12-06 10:27:41 -0800

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,12 @@
 <footer>
-			<p id="footerleft">&copy; 2021 - by Julio Ramos, All Rights Reserved.
-				<br>
-				<a href="https://validator.w3.org/check?uri=julioramos.co/it261" target="_blank">Valid HTML</a> | <a href="http://jigsaw.w3.org/css-validator/validator?uri=julioramos.co/it261" target="_blank">Valid CSS</a>
-			</p>
-		</footer>
+    <ul>
+        <li>Copyright <?php echo date('Y');?></li>
+        <li>All rights reserved</li>
+        <li><a href="../../index.php">Web Design by Julio</a></li>
+		<li><a href="https://validator.w3.org/check?uri=julioramos.co/it261">HTML</a></li>
+        <li><a href="http://jigsaw.w3.org/css-validator/validator?uri=julioramos.co/it261">CSS</a></li>
+    </ul>
+</footer>
 		<!-- END Footer -->
 	</main>
 	<!-- END WRAPPER -->
