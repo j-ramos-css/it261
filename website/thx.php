3c72cc8adc04c5fc7f1f2fa722244e1a2f188dff
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-12-06 15:22:29 -0800

    beer

---
+++ website/thx.php
@@ -0,0 +1,23 @@
+<?php include('includes/header.php'); ?>
+
+<!-- END OF HEAD -->
+
+<div id="wrapper">
+
+    <div id="hero">
+        <img src="images/beer-header.jpg">
+    </div>
+    <!--end hero-->
+
+    <main>
+        <h1>Thank you for subscribing to our newsletter!</h1>
+        <p>We love you</p>
+    </main>
+
+    <aside>
+
+    </aside>
+
+    <!-- BEGINNING OF FOOTER -->
+
+<?php include('includes/footer.php'); ?>
