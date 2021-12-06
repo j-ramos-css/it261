ecb5e85440c465f7e94516dee4b3a301bf55be6b
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-12-05 22:36:57 -0800

    people.php

---
+++ weeks/week8/includes/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <ul>
+        <li>Copyright <?php echo date('Y');?></li>
+        <li>All rights reserved</li>
+        <li><a href="../../index.php">Web Design by Julio</a></li>
+		<li><a href="https://validator.w3.org/check?uri=julioramos.co/it261">HTML</a></li>
+        <li><a href="http://jigsaw.w3.org/css-validator/validator?uri=julioramos.co/it261">CSS</a></li>
+    </ul>
+</footer>
+
+</body>
+</html>
---
+++ weeks/week8/includes/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>People Page</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div id="wrapper">
