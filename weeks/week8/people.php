395efdcfaa046b06b12ae8e4f79701a7c1e2ada7
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-12-05 22:36:42 -0800

    people.php

--- weeks/week8/people.php
+++ weeks/week8/people.php
@@ -6,11 +6,16 @@
 // this needs to be uploaded to server
 
 include('config.php');
-
-// for big database, my header include would go right here.
+include('includes/header.php');
+// for BIG database assignment, my header include would go right here.
 
 // we need to grab our table and assign it to a variable
+?>
 
+<main>
+<h1>Welcome to my people page</h1>
+
+<?php
 $sql = 'SELECT * FROM people';
 
 // we need to connect to database using mysqli_connect() function
@@ -26,12 +31,13 @@ $result = mysqli_query($iConn, $sql) or die(myError(__FILE__,__LINE__,mysqli_err
 if(mysqli_num_rows($result) > 0 ) {
     // now time for the while loop - while loop will return array
     while($row = mysqli_fetch_assoc($result)) {
-        echo '<p>For more information about '.$row['first_name'].', please click <a href="people-view.php?id='.$row['people_id'].'">here.</a></p>';
+        echo '<h3>For more information about '.$row['first_name'].', please click <a href="people-view.php?id='.$row['people_id'].'">here.</a></h3>';
         echo '<ul>';
         echo '<li>'.$row['first_name'].'</li>';
         echo '<li>'.$row['last_name'].'</li>';
         echo '<li>'.$row['email'].'</li>';
         echo '</ul>';
+        echo '<hr>';
     }
 } else {
     echo 'Houston, we have a problem.';
@@ -39,3 +45,15 @@ if(mysqli_num_rows($result) > 0 ) {
 
 mysqli_free_result($result);
 mysqli_close($iConn);
+
+?>
+</main>
+
+<aside>
+
+
+</aside>
+
+</div> <!--end wrapper-->
+
+<?php include('includes/footer.php'); ?>
