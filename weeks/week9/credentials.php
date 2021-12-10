37847b63d41f24d7cda681f85cdbabde5cf1d8b5
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-12-10 15:33:05 -0800

    ignore

--- weeks/week9/credentials.php
+++ weeks/week9/credentials.php
@@ -2,4 +2,4 @@
 define('DB_NAME', 'it261julio');
 define('DB_USER', 'julioramosco');
 define('DB_PASSWORD', 'Commie24');
-define('DB_HOST', 'mysql.julioramos.co'); 
+define('DB_HOST', 'mysql.julioramos.co');
